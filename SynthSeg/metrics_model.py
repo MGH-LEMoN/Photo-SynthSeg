8c83a21dfb2a02e1a1ee17f3035cd88e96ba110a
hvgazula <hvgazula@users.noreply.github.com>
2021-09-09 11:34:04 -0400

    merged SyntheSeg/metrics_model and SynthSeg/model_inputs from master

--- SynthSeg/metrics_model.py
+++ SynthSeg/metrics_model.py
@@ -2,11 +2,10 @@
 import keras.layers as KL
 import numpy as np
 import tensorflow as tf
-from keras.models import Model
-
 # third-party imports
 from ext.lab2im import edit_tensors as l2i_et
 from ext.lab2im import layers, utils
+from keras.models import Model
 
 
 def metrics_model(input_model, label_list, metrics='dice'):
