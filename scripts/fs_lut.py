c764abf8ef9e52935a1f2b016d1b78aa27edb7d5
hvgazula <hvgazula@users.noreply.github.com>
2021-10-18 09:41:31 -0400

    Added fs_lut script for easy look up

---
+++ scripts/fs_lut.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+
+def check_user_input(input):
+    try:
+        val = int(input)
+        return val
+    except ValueError:
+        print('Invalid String')
+        exit()
+
+
+def fs_lut():
+    # Extract contents from look-up table
+    LUT_FILE = '/usr/local/freesurfer/dev/FreeSurferColorLUT.txt'
+    with open(LUT_FILE, 'r') as f:
+        lines = f.read().splitlines()
+
+    filter_lines = [line for line in lines if re.search('^[0-9]', line)]
+    split_lines = [line.split()[:2] for line in filter_lines]
+    idx_to_label = dict(split_lines)
+    label_to_idx = {v: k for k, v in idx_to_label.items()}
+
+    return idx_to_label, label_to_idx
+
+
+if __name__ == '__main__':
+    lut, reverse_lut = fs_lut()
+    idx = check_user_input(sys.argv[1])
+
+    print(lut[str(idx)])
