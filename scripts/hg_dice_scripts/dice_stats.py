e54fc490a1a9614bbb3b49bafa839023bd502e92
hvgazula <hvgazula@users.noreply.github.com>
2021-12-14 18:23:02 -0500

    removed redundant line in dice_stats.py

--- scripts/hg_dice_scripts/dice_stats.py
+++ scripts/hg_dice_scripts/dice_stats.py
@@ -5,18 +5,17 @@ from scipy.stats import norm
 
 
 def fisherZ(r):
-    return (.5 * math.log((1.0 + r) / (1.0 - r)))
+    return 0.5 * math.log((1.0 + r) / (1.0 - r))
 
 
 def calculate_pval(r12, r13, r23, n):
 
     z12 = fisherZ(r12)
     z13 = fisherZ(r13)
-    z23 = fisherZ(r23)
 
     r1sq = ((r12 + r13) / 2.0) * ((r12 + r13) / 2.0)
     variance = (1.0 / ((1 - r1sq) *
-                       (1 - r1sq))) * (r23 * (1.0 - 2.0 * r1sq) - .5 * r1sq *
+                       (1 - r1sq))) * (r23 * (1.0 - 2.0 * r1sq) - 0.5 * r1sq *
                                        (1 - 2.0 * r1sq - (r23 * r23)))
     variance2 = np.sqrt((2.0 - 2.0 * variance) / (n - 3.0))
 
