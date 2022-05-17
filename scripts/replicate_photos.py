b2f92a3de0b6e2bfbee159d3b9124fe4e03f3e05
hvgazula <hvgazula@users.noreply.github.com>
2022-05-17 13:47:42 -0400

    added multiprocesing to replicate photos

--- scripts/replicate_photos.py
+++ scripts/replicate_photos.py
@@ -1,5 +1,7 @@
 import glob
 import os
+from functools import partial
+from multiprocessing import Pool
 from turtle import position
 
 import matplotlib.pyplot as plt
@@ -12,17 +14,10 @@ from tqdm import tqdm
 from ext.hg_utils import zoom
 from ext.lab2im import utils
 
-SKIP = 2
-PRJCT_ID = '4harshaHCP'  # '4harshaHCP'
-PRJCT_DIR = '/space/calico/1/users/Harsha/SynthSeg'
-DATA_DIR = os.path.join(PRJCT_DIR, 'data')
-RESULTS_DIR = os.path.join(PRJCT_DIR, 'results')
-IN_DIR = os.path.join(DATA_DIR, PRJCT_ID)
-OUT_DIR = os.path.join(RESULTS_DIR,
-                       PRJCT_ID + '-skip-02')  # '4harshaHCP_extracts'
+np.random.seed(0)
 
 
-def process_t1(t1_file, t1_name):
+def process_t1(args, t1_file, t1_name):
 
     # 1. Sample 3 rotations about the 3 axes, e.g., between -30 and 30 degrees.
     rotation = np.random.randint(-30, 31, 3)
@@ -38,7 +33,8 @@ def process_t1(t1_file, t1_name):
         shearing=None,
         translation=translation)
 
-    t1_rigid_out = os.path.join(OUT_DIR, t1_name, f'{t1_name}.rigid.npy')
+    t1_rigid_out = os.path.join(args["OUT_DIR"], t1_name,
+                                f'{t1_name}.rigid.npy')
     np.save(t1_rigid_out, t1_rigid_mat)
 
     # 4. Open the T1, and premultiply the affine matrix of the header (“vox2ras”) by the matrix from 3.
@@ -47,14 +43,15 @@ def process_t1(t1_file, t1_name):
     hdr.set_sform(new_aff)
 
     # 5. Binarize the T1 volume by thresholding at 0 and save it with the new header, and call it “mri.mask.mgz”
-    t1_out_path = os.path.join(OUT_DIR, t1_name, f'{t1_name}.mri.mask.mgz')
+    t1_out_path = os.path.join(args["OUT_DIR"], t1_name,
+                               f'{t1_name}.mri.mask.mgz')
     utils.save_volume(volume > 0, new_aff, hdr, t1_out_path)
 
 
-def process_t2(t2_file, t2_name):
+def process_t2(args, t2_file, t2_name, radii=0):
 
-    AFFINE_DIR = os.path.join(OUT_DIR, t2_name, 'slice_affines')
-    PHOTO_DIR = os.path.join(OUT_DIR, t2_name, 'photo_dir')
+    AFFINE_DIR = os.path.join(args["OUT_DIR"], t2_name, 'slice_affines')
+    PHOTO_DIR = os.path.join(args["OUT_DIR"], t2_name, 'photo_dir')
 
     # 6. Create a directory “photo_dir"
     os.makedirs(AFFINE_DIR, exist_ok=True)
@@ -71,8 +68,29 @@ def process_t2(t2_file, t2_name):
     for z in range(Nslices_of_T2):
         curr_slice = t2_vol[..., z]
 
-        if np.sum(curr_slice) and not z % SKIP:
+        if np.sum(curr_slice) and not z % args["SKIP"]:
             c += 1
+
+            if radii:
+                i = 0
+                all_idx = []
+                while True:
+                    if i == 15:
+                        print(all_idx)
+                        print(f'Possiby Bad Case: {t2_name}')
+                        break
+
+                    rand_idx = np.random.choice(np.arange(1, radii + 1))
+                    rand_idx = rand_idx if np.random.random(
+                    ) < 0.5 else -rand_idx
+
+                    curr_slice = t2_vol[..., z + rand_idx]
+
+                    i += 1
+                    all_idx.append(rand_idx)
+
+                    if np.sum(curr_slice):
+                        break
 
             curr_slice = np.pad(np.rot90(curr_slice), 25)
             # Sample a rotation eg between -20 and 20 degrees
@@ -145,44 +163,93 @@ def process_t2(t2_file, t2_name):
             # plt.colorbar(l1, ax=ax[1])
 
 
-def main():
+def sub_pipeline(args, t1_file, t2_file):
+    # get file name
+    t1_fname = os.path.split(t1_file)[1]
+    t2_fname = os.path.split(t2_file)[1]
+
+    # get subject ID
+    t1_subject_name = t1_fname.split('.')[0]
+    t2_subject_name = t2_fname.split('.')[0]
+
+    assert t1_subject_name == t2_subject_name, "Incorrect Subject Name"
+
+    # make output directory for subject
+    out_subject_dir = os.path.join(args["OUT_DIR"], t1_subject_name)
+
+    if os.path.isdir(out_subject_dir):
+        pass
+    else:
+        os.makedirs(out_subject_dir, exist_ok=True)
+
+    # create symlinks to source files (T1, T2)
+    t1_dst = os.path.join(out_subject_dir, t1_fname)
+    t2_dst = os.path.join(out_subject_dir, t2_fname)
+
+    if not os.path.exists(t1_dst):
+        os.symlink(t1_file, t1_dst)
+
+    if not os.path.exists(t2_dst):
+        os.symlink(t2_file, t2_dst)
+
+    # work on T1 and T2 volumes
+    process_t1(args, t1_file, t1_subject_name)
+    process_t2(args, t2_file, t2_subject_name, radii=1)
+
+
+def pipeline(args):
     # list all T1 and T2 files
-    t1_files = sorted(glob.glob(os.path.join(IN_DIR, '*T1.nii.gz')))
-    t2_files = sorted(glob.glob(os.path.join(IN_DIR, '*T2.nii.gz')))
+    t1_files = sorted(glob.glob(os.path.join(args["IN_DIR"], '*T1.nii.gz')))
+    t2_files = sorted(glob.glob(os.path.join(args["IN_DIR"], '*T2.nii.gz')))
 
     assert len(t1_files) == len(t2_files), 'Subject Mismatch'
 
     for i in tqdm(range(len(t1_files)), position=0, leave=True):
         t1_file = t1_files[i]
         t2_file = t2_files[i]
+        sub_pipeline(args, t1_file, t2_file)
 
-        # get file name
-        t1_fname = os.path.split(t1_file)[1]
-        t2_fname = os.path.split(t2_file)[1]
 
-        # get subject ID
-        t1_subject_name = t1_fname.split('.')[0]
-        t2_subject_name = t2_fname.split('.')[0]
+def make_args(skip):
+    PRJCT_ID = '4harshaHCP'  # '4harshaHCP'
+    PRJCT_DIR = '/space/calico/1/users/Harsha/SynthSeg'
+    DATA_DIR = os.path.join(PRJCT_DIR, 'data')
+    RESULTS_DIR = os.path.join(PRJCT_DIR, 'results/hcp-results')
+    IN_DIR = os.path.join(DATA_DIR, PRJCT_ID)
+    OUT_DIR = os.path.join(RESULTS_DIR, PRJCT_ID +
+                           f'-skip-{skip:02d}-r1')  # '4harshaHCP_extracts'
 
-        assert t1_subject_name == t2_subject_name, "Incorrect Subject Name"
+    args = dict(IN_DIR=IN_DIR, OUT_DIR=OUT_DIR, SKIP=skip)
 
-        # make output directory for subject
-        out_subject_dir = os.path.join(OUT_DIR, t1_subject_name)
-        os.makedirs(out_subject_dir, exist_ok=True)
+    return args
 
-        # create symlinks to source files (T1, T2)
-        t1_dst = os.path.join(out_subject_dir, t1_fname)
-        t2_dst = os.path.join(out_subject_dir, t2_fname)
 
-        if not os.path.exists(t1_dst):
-            os.symlink(t1_file, t1_dst)
+def pipeline_wrapper(idx, args, t1_files, t2_files):
+    t1_file = t1_files[idx]
+    t2_file = t2_files[idx]
+    sub_pipeline(args, t1_file, t2_file)
 
-        if not os.path.exists(t2_dst):
-            os.symlink(t2_file, t2_dst)
 
-        # work on T1 and T2 volumes
-        process_t1(t1_file, t1_subject_name)
-        process_t2(t2_file, t2_subject_name)
+def pipeline_mp(args):
+    t1_files = sorted(glob.glob(os.path.join(args["IN_DIR"], '*T1.nii.gz')))
+    t2_files = sorted(glob.glob(os.path.join(args["IN_DIR"], '*T2.nii.gz')))
+
+    assert len(t1_files) == len(t2_files), 'Subject Mismatch'
+
+    with Pool() as pool:
+        pool.map(
+            partial(pipeline_wrapper,
+                    args=args,
+                    t1_files=t1_files,
+                    t2_files=t2_files), range(len(t1_files)))
+
+
+def main():
+    for skip in range(2, 17, 2):
+        print(f'Running Skip {skip:02d}')
+        args = make_args(skip)
+        # pipeline(args)
+        pipeline_mp(args)
 
 
 if __name__ == '__main__':
